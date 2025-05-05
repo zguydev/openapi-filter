f84d45110bfe1dc24dbe168e0ff26e6e4d64c50f
zguy <zguy@zguy.dev>
2025-05-06 02:09:55 +0300

    feat: add logger

--- internal/utils/logger.go
+++ internal/utils/logger.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 
 	"go.uber.org/zap"
+
+	"github.com/zguydev/openapi-filter/internal/config"
 )
 
 func NewFallbackLogger() *zap.Logger {
@@ -23,4 +25,27 @@ func NewFallbackLogger() *zap.Logger {
 		panic(fmt.Errorf("failed to create fallback logger: %w", err))
 	}
 	return logger
+}
+
+func NewLogger(cfg *config.LoggerConfig) (*zap.Logger, error) {
+	level, err := zap.ParseAtomicLevel(cfg.Level)
+	if err != nil {
+		return nil, fmt.Errorf("wrong logger level in config: %w", err)
+	}
+
+	var zapCfg zap.Config
+	switch strings.ToLower(os.Getenv("APP_ENV")) {
+	case "production", "prod":
+		zapCfg = zap.NewProductionConfig()
+	default:
+		zapCfg = zap.NewDevelopmentConfig()
+	}
+	zapCfg.Level = level
+	zapCfg.OutputPaths = []string{"stdout"}
+
+	logger, err := zapCfg.Build()
+	if err != nil {
+		return nil, fmt.Errorf("zapCfg.Build: %w", err)
+	}
+	return logger, nil
 }
