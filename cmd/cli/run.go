4823cfd51e43313df87b89a6d75b29632939e447
zguy <zguy@zguy.dev>
2025-05-06 03:14:02 +0300

    feat: update filter CLI run

--- cmd/cli/run.go
+++ cmd/cli/run.go
@@ -1,9 +1,14 @@
 package cli
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 
+	"github.com/zguydev/openapi-filter/internal/config"
+	"github.com/zguydev/openapi-filter/internal/filter"
 	"github.com/zguydev/openapi-filter/internal/utils"
 )
 
@@ -11,10 +16,27 @@ func run(cmd *cobra.Command, args []string) {
 	fallbackLogger := utils.NewFallbackLogger()
 	defer fallbackLogger.Sync() //nolint:errcheck
 
-	_, err := cmd.Flags().GetString("config")
+	configPath, err := cmd.Flags().GetString("config")
 	if err != nil {
 		fallbackLogger.Fatal("failed to get config flag", zap.Error(err))
 	}
 
-	
+	cfg, err := config.LoadConfig(configPath)
+	if err != nil {
+		fallbackLogger.Fatal("failed to load config",
+			zap.Error(fmt.Errorf("config.LoadConfig: %w", err)))
+	}
+
+	logger, err := utils.NewLogger(cfg.Tool.Logger)
+	if err != nil {
+		fallbackLogger.Fatal("failed to init logger",
+			zap.Error(fmt.Errorf("utils.NewLogger: %w", err)))
+	}
+
+	oaf := filter.NewOpenAPISpecFilter(cfg, logger)
+	if err := oaf.Filter(args[0], args[1]); err != nil {
+		logger.Error("filter on spec failed",
+			zap.Error(fmt.Errorf("oaf.Filter: %w", err)))
+		os.Exit(1)
+	}
 }
