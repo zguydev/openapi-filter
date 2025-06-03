90a17323b2bd8b9e0e099fd2cf0f14ee325b1a79
zguy <zguy@zguy.dev>
2025-06-04 02:26:13 +0300

    feat: add spec io

---
+++ internal/filter/spec_io.go
@@ -0,0 +1,39 @@
+package filter
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"gopkg.in/yaml.v3"
+)
+
+func loadSpecFromFile(loader *openapi3.Loader, specPath string) (*openapi3.T, error) {
+	doc, err := loader.LoadFromFile(specPath)
+	if err != nil {
+		return nil, fmt.Errorf("oaf.loader.LoadFromFile: %w", err)
+	}
+	return doc, nil
+}
+
+func writeSpecToFile(doc *openapi3.T, specPath string) error {
+	yamlData, err := doc.MarshalYAML()
+	if err != nil {
+		return fmt.Errorf("doc.MarshalYAML: %w", err)
+	}
+
+	outputFile, err := os.Create(specPath)
+	if err != nil {
+		return fmt.Errorf("os.Create: %w", err)
+	}
+	defer outputFile.Close() //nolint:errcheck
+
+	encoder := yaml.NewEncoder(outputFile)
+	encoder.SetIndent(2)
+	defer encoder.Close() //nolint:errcheck
+
+	if err := encoder.Encode(yamlData); err != nil {
+		return fmt.Errorf("encoder.Encode: %w", err)
+	}
+	return nil
+}
