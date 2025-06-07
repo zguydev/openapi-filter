400633c5165a4128a33834a3d4d12bb36663a767
zguy <zguy@zguy.dev>
2025-06-07 19:42:21 +0300

    refactor: update refs collector

--- internal/filter/refs_collector.go
+++ internal/filter/refs_collector.go
@@ -21,15 +21,15 @@ func (rc *RefsCollector) Refs() map[string]struct{} {
 }
 
 func (rc *RefsCollector) CollectOperation(op *openapi3.Operation) {
-	rc.CollectParameters(op.Parameters)
+	rc.collectParameters(op.Parameters)
 	if op.RequestBody != nil {
-		rc.CollectRequestBodyRef(op.RequestBody)
+		rc.collectRequestBodyRef(op.RequestBody)
 	}
-	rc.CollectResponses(op.Responses)
-	rc.CollectCallbacks(op.Callbacks)
+	rc.collectResponses(op.Responses)
+	rc.collectCallbacks(op.Callbacks)
 }
 
-func (rc *RefsCollector) CollectParameters(params openapi3.Parameters) {
+func (rc *RefsCollector) collectParameters(params openapi3.Parameters) {
 	for _, param := range params {
 		if param.Ref != "" {
 			rc.AddRef(param.Ref)
@@ -46,15 +46,24 @@ func (rc *RefsCollector) collectParameter(param *openapi3.Parameter) {
 	rc.collectContent(param.Content)
 }
 
-func (rc *RefsCollector) collectSchemaRef(sr *openapi3.SchemaRef) {
-	if sr == nil {
+func (rc *RefsCollector) collectParameterRef(paramr *openapi3.ParameterRef) {
+	if paramr.Ref != "" {
+		rc.AddRef(paramr.Ref)
+	}
+	if p := paramr.Value; p != nil {
+		rc.collectParameter(p)
+	}
+}
+
+func (rc *RefsCollector) collectSchemaRef(scr *openapi3.SchemaRef) {
+	if scr == nil {
 		return
 	}
-	if sr.Ref != "" {
-		rc.AddRef(sr.Ref)
+	if scr.Ref != "" {
+		rc.AddRef(scr.Ref)
 	}
-	if sr.Value != nil {
-		rc.collectSchema(sr.Value)
+	if scr.Value != nil {
+		rc.collectSchema(scr.Value)
 	}
 }
 
@@ -80,18 +89,22 @@ func (rc *RefsCollector) collectEncoding(enc *openapi3.Encoding) {
 	rc.collectHeaders(enc.Headers)
 }
 
-func (rc *RefsCollector) collectHeaders(headers openapi3.Headers) {
-	for _, header := range headers {
-		if header.Ref != "" {
-			rc.AddRef(header.Ref)
-		}
-		if h := header.Value; h != nil {
-			rc.collectParameter(&h.Parameter) // Header type embeds the Parameter type
-		}
+func (rc *RefsCollector) collectHeaderRef(hr *openapi3.HeaderRef) {
+	if hr.Ref != "" {
+		rc.AddRef(hr.Ref)
+	}
+	if h := hr.Value; h != nil {
+		rc.collectParameter(&h.Parameter) // Header type embeds the Parameter type
 	}
 }
 
-func (rc *RefsCollector) CollectRequestBodyRef(rbr *openapi3.RequestBodyRef) {
+func (rc *RefsCollector) collectHeaders(headers openapi3.Headers) {
+	for _, hr := range headers {
+		rc.collectHeaderRef(hr)
+	}
+}
+
+func (rc *RefsCollector) collectRequestBodyRef(rbr *openapi3.RequestBodyRef) {
 	if rbr.Ref != "" {
 		rc.AddRef(rbr.Ref)
 	}
@@ -104,16 +117,20 @@ func (rc *RefsCollector) collectRequestBodyRefs(rb *openapi3.RequestBody) {
 	rc.collectContent(rb.Content)
 }
 
-func (rc *RefsCollector) CollectResponses(resps *openapi3.Responses) {
-	for _, resp := range resps.Map() {
-		if resp.Ref != "" {
-			rc.AddRef(resp.Ref)
-		}
-		if r := resp.Value; r != nil {
-			rc.collectHeaders(r.Headers)
-			rc.collectContent(r.Content)
-			rc.collectLinks(r.Links)
-		}
+func (rc *RefsCollector) collectResponseRef(respr *openapi3.ResponseRef) {
+	if respr.Ref != "" {
+		rc.AddRef(respr.Ref)
+	}
+	if r := respr.Value; r != nil {
+		rc.collectHeaders(r.Headers)
+		rc.collectContent(r.Content)
+		rc.collectLinks(r.Links)
+	}
+}
+
+func (rc *RefsCollector) collectResponses(resps *openapi3.Responses) {
+	for _, respr := range resps.Map() {
+		rc.collectResponseRef(respr)
 	}
 }
 
@@ -130,16 +147,20 @@ func (rc *RefsCollector) collectLinkRef(lr *openapi3.LinkRef) {
 	// TODO: add OperationRefs handling
 }
 
-func (rc *RefsCollector) CollectCallbacks(callbacks openapi3.Callbacks) {
-	for _, callback := range callbacks {
-		if callback.Ref != "" {
-			rc.AddRef(callback.Ref)
+func (rc *RefsCollector) collectCallbackRef(cbr *openapi3.CallbackRef) {
+	if cbr.Ref != "" {
+		rc.AddRef(cbr.Ref)
+	}
+	if c := cbr.Value; c != nil {
+		for _, path := range c.Map() {
+			rc.collectPathItem(path)
 		}
-		if c := callback.Value; c != nil {
-			for _, path := range c.Map() {
-				rc.collectPathItem(path)
-			}
-		}
+	}
+}
+
+func (rc *RefsCollector) collectCallbacks(callbacks openapi3.Callbacks) {
+	for _, cbr := range callbacks {
+		rc.collectCallbackRef(cbr)
 	}
 }
 
@@ -150,29 +171,41 @@ func (rc *RefsCollector) collectPathItem(path *openapi3.PathItem) {
 	for _, op := range path.Operations() {
 		rc.CollectOperation(op)
 	}
-	rc.CollectParameters(path.Parameters)
+	rc.collectParameters(path.Parameters)
 }
 
-func (rc *RefsCollector) collectSchemaRefs(s openapi3.SchemaRefs) {
-	for _, sr := range s {
+func (rc *RefsCollector) collectSchemaRefs(scrs openapi3.SchemaRefs) {
+	for _, sr := range scrs {
 		rc.collectSchemaRef(sr)
 	}
 }
 
-func (rc *RefsCollector) collectSchemas(s openapi3.Schemas) {
-	for _, sr := range s {
+func (rc *RefsCollector) collectSchemas(scs openapi3.Schemas) {
+	for _, sr := range scs {
 		rc.collectSchemaRef(sr)
 	}
 }
 
-func (rc *RefsCollector) collectSchema(s *openapi3.Schema) {
-	rc.collectSchemaRefs(s.OneOf)
-	rc.collectSchemaRefs(s.AnyOf)
-	rc.collectSchemaRefs(s.AllOf)
-	rc.collectSchemaRef(s.Not)
+func (rc *RefsCollector) collectSchema(sc *openapi3.Schema) {
+	rc.collectSchemaRefs(sc.OneOf)
+	rc.collectSchemaRefs(sc.AnyOf)
+	rc.collectSchemaRefs(sc.AllOf)
+	rc.collectSchemaRef(sc.Not)
 
-	rc.collectSchemaRef(s.Items)
+	rc.collectSchemaRef(sc.Items)
 
-	rc.collectSchemas(s.Properties)
-	rc.collectSchemaRef(s.AdditionalProperties.Schema)
+	rc.collectSchemas(sc.Properties)
+	rc.collectSchemaRef(sc.AdditionalProperties.Schema)
+}
+
+func (rc *RefsCollector) collectSecurityScheme(secsc *openapi3.SecuritySchemeRef) {
+	if secsc.Ref != "" {
+		rc.AddRef(secsc.Ref)
+	}
+}
+
+func (rc *RefsCollector) collectExampleRef(exr *openapi3.ExampleRef) {
+	if exr.Ref != "" {
+		rc.AddRef(exr.Ref)
+	}
 }
